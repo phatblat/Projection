eaf1b26c09689249c4c780b2a4396018b2f3c956
Ben Chatelain <ben@octop.ad>
2016-05-08 11:18:28 -0600

    Add iOS framework target

---
+++ Projection-iOS/Projection-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Projection-iOS.h
+//  Projection-iOS
+//
+//  Created by Ben Chatelain on 5/8/16.
+//  Copyright © 2016 Ben Chatelain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Projection-iOS.
+FOUNDATION_EXPORT double Projection_iOSVersionNumber;
+
+//! Project version string for Projection-iOS.
+FOUNDATION_EXPORT const unsigned char Projection_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Projection_iOS/PublicHeader.h>
+
+
