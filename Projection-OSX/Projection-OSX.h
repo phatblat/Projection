feb71cf2b55054452ff611564fd1862f94708dec
Ben Chatelain <ben@octop.ad>
2016-05-08 11:17:36 -0600

    Create project and OSX framework target

---
+++ Projection-OSX/Projection-OSX.h
@@ -0,0 +1,19 @@
+//
+//  Projection-OSX.h
+//  Projection-OSX
+//
+//  Created by Ben Chatelain on 5/8/16.
+//  Copyright © 2016 Ben Chatelain. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Projection-OSX.
+FOUNDATION_EXPORT double Projection_OSXVersionNumber;
+
+//! Project version string for Projection-OSX.
+FOUNDATION_EXPORT const unsigned char Projection_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Projection_OSX/PublicHeader.h>
+
+
